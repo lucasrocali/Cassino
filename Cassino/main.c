78e3b5cf37c146ee4fc5a7ecb199a2e7bfdec717
Lucas Rocali <rocali@Lucass-MBP.lan>
2015-04-15 02:20:46 +1000

    Initial Commit

---
+++ Cassino/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Cassino
+//
+//  Created by Lucas Rocali on 4/15/15.
+//  Copyright (c) 2015 Lucas Rocali. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
